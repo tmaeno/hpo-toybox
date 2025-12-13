689cdb661ce02eed0a5ebd4d9bdfc4ab8aea8752
tmaeno <tmaeno@bnl.gov>
2025-12-13 23:12:45 +0100

    Enhance downsize.py with progress reporting and failure handling

--- src/downsize.py
+++ src/downsize.py
@@ -36,75 +36,103 @@ def move(dest, cur_dir, prefix):
             shutil.rmtree(os.path.join(cur_dir, sub_dir))
 
 
+def extract_sub_archives(cur_dir):
+    for _i in os.listdir(cur_dir):
+        tmp_path = os.path.join(cur_dir, _i)
+        if os.path.isdir(tmp_path):
+            extract_sub_archives(tmp_path)
+        elif tmp_path.endswith('.rar') or tmp_path.endswith('.zip'):
+            extract_to = tmp_path.rsplit('.', 1)[0]
+            os.mkdir(extract_to)
+            if tmp_path.endswith('.rar'):
+                with rarfile.RarFile(tmp_path) as rf:
+                    rf.extractall(extract_to)
+            else:
+                with zipfile.ZipFile(tmp_path) as zf:
+                    zf.extractall(extract_to)
+            os.remove(tmp_path)
+            extract_sub_archives(extract_to)
+
+
 while True:
-    full_name = input("Enter file name: ").strip()
+    full_input = input("Enter file name: ").strip()
+    full_name_list = full_input.split(' /')
+    for full_name in full_name_list:
+        full_name = full_name.strip()
+        if not full_name.startswith("/"):
+            full_name = "/" + full_name
 
-    old_filename = os.path.basename(full_name).replace('\\', '')
-    base_dir = os.path.dirname(full_name)
+        old_filename = os.path.basename(full_name).replace('\\', '')
+        base_dir = os.path.dirname(full_name)
 
-    target_folder_name = old_filename.split('.')[0]
+        target_folder_name = old_filename.split('.')[0]
 
-    os.chdir(base_dir)
-    if os.path.exists(target_folder_name):
-        shutil.rmtree(target_folder_name)
-    os.mkdir(target_folder_name)
+        os.chdir(base_dir)
+        if os.path.exists(target_folder_name):
+            shutil.rmtree(target_folder_name)
+        os.mkdir(target_folder_name)
 
-    if old_filename.endswith('.rar'):
-        with rarfile.RarFile(old_filename) as rf:
-            rf.extractall(target_folder_name)
-    elif old_filename.endswith('.zip'):
-        with zipfile.ZipFile(old_filename) as zf:
-            zf.extractall(target_folder_name)
-    else:
-        print(f"Unsupported file format: \"{old_filename}\"")
-        sys.exit(1)
+        if old_filename.endswith('.rar'):
+            with rarfile.RarFile(old_filename) as rf:
+                rf.extractall(target_folder_name)
+        elif old_filename.endswith('.zip'):
+            with zipfile.ZipFile(old_filename) as zf:
+                zf.extractall(target_folder_name)
+        else:
+            print(f"Unsupported file format: \"{old_filename}\"")
+            sys.exit(1)
 
-    remove_redundant_sub_dirs(target_folder_name)
-    move(target_folder_name, target_folder_name, "base")
+        extract_sub_archives(target_folder_name)
+        remove_redundant_sub_dirs(target_folder_name)
+        move(target_folder_name, target_folder_name, "base")
 
-    resize_factor = 0.8
-    quality = 80
-    max_height = 1200
+        resize_factor = 0.8
+        quality = 80
+        max_height = 1200
 
-    f_list = os.listdir(target_folder_name)
-    n_failures = 0
-    for i, f in enumerate(f_list):
-        if i % 20 == 0:
-            print(f"  Processed: {i}/{len(f_list)}")
-        input_path = os.path.join(target_folder_name, f)
-        output_path = input_path + '.new.jpg'
-        is_failed = True
-        try:
-            with Image.open(input_path) as img:
-                if img.mode == 'RGBA':
-                    img = img.convert('RGB')
-                height = int(min(img.height * 0.8, max_height))
-                width = int((height / img.height) * img.width)
-                img = img.resize((width, height), Image.Resampling.LANCZOS)
-                enhancer = ImageEnhance.Sharpness(img)
-                img = enhancer.enhance(1.1)
-                img.save(output_path, "JPEG", quality=quality, optimize=True)
-                is_failed = False
-        except UnidentifiedImageError as e:
-            pass
-        except Exception as e:
-            print(f"Error processing {input_path}: {e}")
-        os.remove(input_path)
-        if is_failed:
-            n_failures += 1
+        f_list = os.listdir(target_folder_name)
+        n_failures = 0
+        for i, f in enumerate(f_list):
+            if i % 20 == 0:
+                print(f"  Processed: {i}/{len(f_list)}")
+            input_path = os.path.join(target_folder_name, f)
+            output_path = input_path + '.new.jpg'
+            is_failed = True
+            try:
+                with Image.open(input_path) as img:
+                    if img.mode == 'RGBA':
+                        img = img.convert('RGB')
+                    height = int(min(img.height * 0.8, max_height))
+                    width = int((height / img.height) * img.width)
+                    img = img.resize((width, height), Image.Resampling.LANCZOS)
+                    enhancer = ImageEnhance.Sharpness(img)
+                    img = enhancer.enhance(1.1)
+                    img.save(output_path, "JPEG", quality=quality, optimize=True)
+                    is_failed = False
+            except UnidentifiedImageError as e:
+                pass
+            except Exception as e:
+                print(f"Error processing {input_path}: {e}")
+            os.remove(input_path)
+            if is_failed:
+                n_failures += 1
 
-    output_zip = old_filename.rsplit('.', 1)[0] + '.zip'
+        tmp_output_zip = old_filename.rsplit('.', 1)[0] + ".new.zip"
+        output_zip = old_filename.rsplit('.', 1)[0] + ".zip"
 
-    with zipfile.ZipFile(output_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
-        # Walk through the source directory and add all files
-        for root, _, files in os.walk(target_folder_name):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, arcname=file)
+        with zipfile.ZipFile(tmp_output_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
+            # Walk through the source directory and add all files
+            for root, _, files in os.walk(target_folder_name):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, arcname=file)
 
-    shutil.rmtree(target_folder_name)
-    if n_failures / len(f_list) > 0.2:
-        print("More than 20% failures, something went wrong. Keeping original file")
-    else:
-        os.remove(old_filename)
+        max_failures = 0.2
+        if n_failures / len(f_list) > max_failures:
+            print(f"More than {int(max_failures*100)}% failures, something went wrong. Keeping original file")
+        else:
+            shutil.rmtree(target_folder_name)
+            os.remove(old_filename)
+            os.rename(tmp_output_zip, output_zip)
+
 
